a6be181c291db21fa31b1230e098860c79094127
Naveen Kumar <naveen.umar@jalantechnologies.com>
2022-03-17 22:49:10 +0530

    setup index

--- controllers/user-controller.ts
+++ controllers/user-controller.ts
@@ -1,16 +1,18 @@
-import express, { NextFunction, Request, Response } from "express";
-import * as userService from "../services/user-service";
+import { NextFunction, Request, Response } from "express";
+import { userService } from "../services";
 
-const router = express.Router();
-
-router.get("/", async (req: Request, res: Response, next: NextFunction) => {
+const helloFunc = async (req: Request, res: Response, next: NextFunction) => {
   const message = await userService.helloFunction();
   return res.status(200).send(message);
-});
+};
 
-router.get("/other",  async (req: Request, res: Response, next: NextFunction) => {
+const otherHelloFunc = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const message = await userService.otherHelloFuncion();
   return res.status(200).send(message);
-})
+};
 
-module.exports = router;
+export { helloFunc, otherHelloFunc };
